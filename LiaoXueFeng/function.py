b4ee4dbf9aa87b1ad76d515641742e599ea06016
MiracleWong <cfwr1991@126.com>
2017-07-11 14:34:55 +0800

    use the module

--- LiaoXueFeng/function.py
+++ LiaoXueFeng/function.py
@@ -53,3 +53,17 @@ L2 = []
 for x in xrange(1,11):
     L2.append(x*x)
 print L2
+
+
+print [x * x for x in range(1, 11)]
+print [x * x for x in range(1, 11) if x % 2 == 0]
+print [m + n for m in 'ABC' for n in 'XYZ']
+
+import os
+print [d for d in os.listdir('.')] # os.listdir可以列出文件和目录
+d = {'x': 'A', 'y': 'B', 'z': 'C' }
+for k, v in d.iteritems():
+    print k, '=', v
+print [k + '=' + v for k, v in d.iteritems()]
+L = ['Hello', 'World', 'IBM', 'Apple']
+print [s.lower() for s in L]
