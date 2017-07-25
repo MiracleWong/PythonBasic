e96bb0dd18b9c7d43ea4de93d6f0b4a8e1a93a8f
MiracleWong <cfwr1991@126.com>
2017-07-25 10:03:28 +0800

    Python Update the MySQL data

--- mysql/python_mysql5.py
+++ mysql/python_mysql5.py
@@ -6,30 +6,21 @@
 import MySQLdb
 
 # 打开数据库连接
-conn = MySQLdb.connect("localhost","root","123456","crm" )
+conn = MySQLdb.connect("localhost", "root", "123456", "crm")
 
 # 使用cursor()方法获取操作游标 
 cursor = conn.cursor()
 
-# SQL 查询语句
-sql = "SELECT * FROM EMPLOYEE WHERE INCOME > '%d'" % (1000)
+# SQL 更新语句
+sql = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
 try:
     # 执行SQL语句
     cursor.execute(sql)
-    # 获取所有记录列表
-    results = cursor.fetchall()
-    print "数据库中取出的数据的样式："
-    print results
-    for row in results:
-        fname = row[0]
-        lname = row[1]
-        age = row[2]
-        sex = row[3]
-        income = row[4]
-        # 打印结果
-        print "fname=%s,lname=%s,age=%d,sex=%s,income=%d" % (fname, lname, age, sex, income )
+    # 提交到数据库执行
+    conn.commit()
+
 except:
-   print "Error: unable to fecth data"
+   conn.rollback()
 
 # 关闭数据库连接
 conn.close()
