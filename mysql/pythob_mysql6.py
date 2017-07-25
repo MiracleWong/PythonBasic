06f8d8ba06840c7202a91ab52d69f6da11290711
MiracleWong <cfwr1991@126.com>
2017-07-25 10:17:47 +0800

    Delete the data

--- mysql/pythob_mysql6.py
+++ mysql/pythob_mysql6.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-# 对数据库的更新操作
+# 对数据库的删除操作
 
 import MySQLdb
 
 # 打开数据库连接
 conn = MySQLdb.connect("localhost", "root", "123456", "crm")
 
-# 使用cursor()方法获取操作游标 
+# 使用cursor()方法获取操作游标
 cursor = conn.cursor()
 
 # SQL 更新语句
-sql = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
+sql = "DELETE  FROM EMPLOYEE  WHERE AGE = '%d'" % (21)
+#sql = "DELETE  FROM EMPLOYEE  WHERE AGE > '%d'" %  (21)
 try:
     # 执行SQL语句
     cursor.execute(sql)
@@ -20,6 +21,8 @@ try:
     conn.commit()
 
 except:
+
+   # 发生错误时进行回滚
    conn.rollback()
 
 # 关闭数据库连接
