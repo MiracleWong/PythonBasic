dcc6ba3558696ba73d8e00f820eb8941b2d48b51
MiracleWong <cfwr1991@126.com>
2017-09-04 19:56:47 +0800

    test Console-Logging

---
+++ testConsoleLoggin.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+from console_logging import console
+
+console.log("Hello World!")
+console.log("This is a log.")
+console.error("This is an error.")
+console.info("This is some neutral info.")
+console.success("This is a success message.")
