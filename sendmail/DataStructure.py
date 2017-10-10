7155e1cc7b38f8a1a2316731c1bda84e655a5cea
MiracleWong <cfwr1991@126.com>
2017-10-10 19:21:52 +0800

    Add the DataStructure.py

---
+++ sendmail/DataStructure.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+
+# List
+Weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+print(Weekday[0])
+
+## List正负索引的使用
+periodic_table = ['H','He','Li','Be','B','C','N','O','F','Ne']
+print(periodic_table[0])
+print(periodic_table[-2])
+print(periodic_table[0:3])
+print(periodic_table[-10:-7])
+print(periodic_table[-10:])
+print(periodic_table[:9])
+
+# 字典Dictonary
+
+## 字典的特征
+
+NASDAQ_CODE = {
+    'BIDU': 'Baidu',
+    'SINA':'Sina',
+    'YOKU': 'Youku'
+}
