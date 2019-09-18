b913b1810b31d39f9bd09525c5817b05b494bc4d
MiracleWong <cfwr1991@126.com>
2019-09-18 21:18:14 +0800

    廖雪峰的Python教程——高级特性 之 列表生成式

--- LiaoXueFeng/advanced_features.py
+++ LiaoXueFeng/advanced_features.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'MiracleWong'
+import os
 from collections import Iterable
 
 
@@ -111,3 +112,38 @@ elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
     print('测试失败!')
 else:
     print('测试成功!')
+
+
+# 3 列表生成式
+print(list(range(1, 11)))
+
+L = []
+for x in range(1, 11):
+    L.append(x*x)
+print(L)
+
+print([x * x for x in range(1, 11)])
+print([x * x for x in range(1, 11) if x % 2 == 0])
+
+print([m + n for m in 'ABC' for n in 'XYZ'])
+
+print([d for d in os.listdir('.')])
+
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
+for k, v in d.items():
+    print(k, '=', v)
+
+print([k + '=' + v for k, v in d.items()])
+
+L = ['Hello', 'World', 'IBM', 'Apple']
+print([s.lower() for s in L])
+
+
+# Practice
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
