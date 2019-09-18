8c60392a795094110e0da887dfb8556213de991d
MiracleWong <cfwr1991@126.com>
2019-09-18 22:08:42 +0800

    廖雪峰的Python教程——高级特性 之 迭代器

--- LiaoXueFeng/advanced_features.py
+++ LiaoXueFeng/advanced_features.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'MiracleWong'
 import os
-from collections import Iterable
+from collections import Iterable, Iterator
 
 
 # 1 切片
@@ -231,3 +231,33 @@ if results == [
     print('测试通过!')
 else:
     print('测试失败!')
+
+
+# 5 迭代器
+
+# 判断是否是Iterable对象
+print(isinstance([], Iterable))
+print(isinstance((), Iterable))
+print(isinstance('abc', Iterable))
+print(isinstance((x for x in range(10)), Iterable))
+print(isinstance(100, Iterable))
+
+
+# 判断一个对象是否是Iterator对象：
+print(isinstance([], Iterator))
+print(isinstance((), Iterator))
+print(isinstance('abc', Iterator))
+print(isinstance((x for x in range(10)), Iterator))
+print(isinstance(100, Iterator))
+
+
+"""
+小结
+凡是可作用于for循环的对象都是Iterable类型；
+
+凡是可作用于next()函数的对象都是Iterator类型，它们表示一个惰性计算的序列；
+
+集合数据类型如list、dict、str等是Iterable但不是Iterator，不过可以通过iter()函数获得一个Iterator对象。
+
+Python的for循环本质上就是通过不断调用next()函数实现的
+"""
