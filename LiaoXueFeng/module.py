7f8cffbee69fdc8c14ab7479275fbbbc912efbb3
MiracleWong <cfwr1991@126.com>
2019-09-19 22:19:26 +0800

    廖雪峰的Python教程——模块, 函数式编程 之 高阶函数 之 map/reduce

--- LiaoXueFeng/module.py
+++ LiaoXueFeng/module.py
@@ -1,4 +1,43 @@
-def main():
-    print "We are in %s" %__name__
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a test module '
+
+__author__ = 'Michael Liao'
+
+import sys
+
+
+def test():
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello, world!')
+    elif len(args) == 2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+
+
+def _private_1(name):
+    return 'Hello, %s' % name
+
+
+def _private_2(name):
+    return 'Hi, %s' % name
+
+
+def greeting(name):
+    if len(name) > 3:
+        return _private_1(name)
+    else:
+        return _private_2(name)
+
+
 if __name__ == '__main__':
-    main()
+    test()
+
+# Notes
+# sys模块有一个argv变量，用list存储了命令行的所有参数。argv至少有一个元素，因为第一个参数永远是该.py文件的名称
+# 类似__xxx__这样的变量是特殊变量，可以被直接引用，但是有特殊用途，比如上面的__author__，__name__就是特殊变量，hello模块定义的文档注释也可以用特殊变量__doc__访问，我们自己的变量一般不要用这种变量名；
+# 类似_xxx和__xxx这样的函数或变量就是非公开的（private），不应该被直接引用，比如_abc，__abc等；
+# 外部不需要引用的函数全部定义成private，只有外部需要引用的函数才定义为public。
