ef148b6b3ad0d70c2bbf85f1db6461d88a46d82c
MiracleWong <cfwr1991@126.com>
2019-09-21 21:38:58 +0800

    廖雪峰的Python教程—-常见内建模块 之 base64

---
+++ LiaoXueFeng/do_base64.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import base64
+print(base64.b64encode(b'binary\x00string'))
+print(base64.b64decode(b'YmluYXJ5AHN0cmluZw=='))
+
+# url safe
+print(base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff'))
+print(base64.urlsafe_b64decode(b'abcd--__'))
