638cb109230053ec9a524272dab07c7a8c5db5cd
MiracleWong <cfwr1991@126.com>
2019-10-07 17:52:57 +0800

    廖雪峰Python教程——常用内建模块

---
+++ LiaoXueFeng/buildin/use_urllib.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'MiracleWong'
+from urllib import request
+
+with request.urlopen('http://news-at.zhihu.com/api/4/news/latest') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))
